b1beb103ed5ac848870a0a351c87beaa29e873fc
DSpiridonova <Dafna.svetlova.83@gmail.com>
2022-08-10 16:47:25 +0300

    Add picture and update test_automation_practice_form.py

---
+++ tests/test_automation_practice_form.py
@@ -0,0 +1,32 @@
+from selene import have, command
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s, ss
+
+
+def test_practice_form():
+
+    browser.open('/automation-practice-form')
+
+#General info
+    s('#firstName').type('Lacie')
+    s('#lastName').type('Lyn')
+    s('#userEmail').type('Lyn@mail.com')
+    # s('//*[contains(@class, "custom-radio")][contains (.//text(), "Female")]')
+    ss('.custom-radio').element_by(have.exact_text('Female')).click()
+    s('#userNumber').type('199966633')
+
+#Date of Birth
+    s('#dateOfBirthInput').click()
+    s('.react-datepicker__month-select').type('January')
+    s('.react-datepicker__year-select').type('1993')
+    s('.react-datepicker__day--013').click()
+
+#Subjects
+    s('#subjectsInput').type('English').press_enter()
+    s('#subjectsInput').type('Computer Science').press_enter()
+
+#Hobbies
+    ss('.custom-checkbox').element_by(have.exact_text('Reading')).click()
+    ss('.custom-checkbox').element_by(have.exact_text('Music')).click()
+
+#Picture
